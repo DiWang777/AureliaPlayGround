bd2e70685661cb21b3deb031d8f31b5bfde20908
Matthew Reid <matthew.reid@lentune.co.nz>
2017-07-07 09:00:16 +1200

    Added kendo grid control

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
+import * as $ from 'jquery';
+import 'kendo/js/kendo.grid';
+
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
+    .plugin('aurelia-kendoui-bridge')
     .feature('resources');
+    
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
--- src/page-one.ts
+++ src/page-one.ts
@@ -1,24 +1,39 @@
 export class PageOne {
-
+    
     email: string = "testEmail";
-
-
-
-  created() {
-      console.debug("PageOne created");
-  }
-
-  activate(context) {
-      this.email = context.id;
-  }
-
-  attached() {
-      console.debug("PageOne attached");
-  }
-
-  detached() {
-      console.debug("PageOne detached");
-  }
-
-
+    datasource:any;
+    
+    constructor(){
+        this.datasource = {
+            data:[
+            {ContactName:'test'},
+            {ContactName:'test 111'},
+            {ContactName:'test 222'},
+            {ContactName:'test 333'},
+            {ContactName:'test 444'},
+            {ContactName:'test 555'},
+            {ContactName:'test 666'},
+            ],
+            pageSize: 5
+        };
+    }
+    
+    
+    created() {
+        console.debug("PageOne created");
+    }
+    
+    activate(context) {
+        this.email = context.id;
+    }
+    
+    attached() {
+        console.debug("PageOne attached");
+    }
+    
+    detached() {
+        console.debug("PageOne detached");
+    }
+    
+    
 }
